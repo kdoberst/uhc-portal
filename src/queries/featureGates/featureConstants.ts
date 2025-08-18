1ad4752e54300e5ed769e8af0a2ae4161dc6aca1
Kim Doberstein <kdoberst@redhat.com>
2025-08-18 10:20:12 -0500

    OCMUI-3431 add AWS tags to new node pools

--- src/queries/featureGates/featureConstants.ts
+++ src/queries/featureGates/featureConstants.ts
@@ -39,6 +39,8 @@ export const AWS_TAGS_NEW_MP = 'ocmui-aws-tags-new-mp';
 export const TABBED_MACHINE_POOL_MODAL = 'ocmui-tabbed-machine-pool-modal';
 export const ROSA_ARCHITECTURE_RENAMING_ALERT = 'ocmui-rosa-architecture-renaming-alert';
 
+export const ENABLE_AWS_TAGS_EDITING = false;
+
 export default {
   AUTO_CLUSTER_TRANSFER_OWNERSHIP,
   HYPERSHIFT_WIZARD_FEATURE,
